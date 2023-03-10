f0fdc4527da0c93b05e8491eb374f633a4484fab
codescalper <msfunbook@gmail.com>
2023-03-10 12:51:47 +0530

    Added api Info

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,10 +1,44 @@
 // Full code on https://github.com/codescalper/weather-app-react
 // Author [Mayank Singh]
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const Weather = () => {
+  const [searchValue, setSearchValue] = useState("Mumbai");
+  const [tempInfo, setTempInfo] = useState({});
+
+  async function getWeatherInfo() {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=7508fdcdd46fd4e4050a5e8cbbdb3722`;
+      let response = await fetch(url);
+      let data = await response.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country
+      };
+      setTempInfo(myNewWeatherInfo);
+      console.log(temp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
   return (
     <>
       <div className="wrap">
@@ -14,9 +48,13 @@ const Weather = () => {
             placeholder="Search"
             autoFocus
             className="searchName"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
           />
 
-          <button className="searchButton">Search</button>
+          <button className="searchButton" onClick={getWeatherInfo}>
+            Search
+          </button>
         </div>
       </div>
       <div className="widget">
