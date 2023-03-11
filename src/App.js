b7d4056d68c8b3a951c0869a984c5aca65c66da6
codescalper <msfunbook@gmail.com>
2023-03-11 19:02:20 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Weather from "./components/weather";
+import Footer from "./components/Footer";
 // import "./App.css";
 
 function App() {
-  return <Weather />;
+  return(
+    <div>
+      <Weather />
+      <Footer />
+    </div>
+    
+  );
 }
 
 export default App;
