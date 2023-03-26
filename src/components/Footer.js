e79bdd15333dfe559f184014cddf6208753457e4
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-03-26 19:22:20 +0530

    Updated the personal website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
       <a href="https://github.com/codescalper" target="_blank"><FaGithub className="icon-git" /></a>
       <a href="https://www.youtube.com/@mhtcetshalamayanksingh" target="_blank"><FaYoutube className="icon-yt" /></a>
       <a href="https://www.linkedin.com/in/mayank-singh-613a41183/" target="_blank"><FaLinkedin className="icon-ldin" /></a>
-      <a href="" target="_blank"><FaGlobe className="icon-wesbite" /></a>
+      <a href="https://mayankonweb.pages.dev/" target="_blank"><FaGlobe className="icon-wesbite" /></a>
     </footer>
   );
 }
